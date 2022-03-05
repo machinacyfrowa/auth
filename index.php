fae4ce1fc822e3c42def3270980f295314673861
machinacyfrowa <38609429+machinacyfrowa@users.noreply.github.com>
2022-03-05 09:52:50 +0100

    verify email & password in database

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+if(isset($_REQUEST['email']) && isset($_REQUEST['password'])) {
+    $email = $_REQUEST['email'];
+    $password = $_REQUEST['password'];
+    
+    $email = filter_var($email, FILTER_SANITIZE_EMAIL);
+    
+    //obiektowo
+    $db = new mysqli("localhost", "root", "", "auth");
+    
+    //strukturalnie 
+    //$d = mysqli_connect("localhost", "root", "", "auth");
+    //mysqli_query($d, "SELECT * FROM user");
+    
+    
+    //ręcznie:
+    //$q = "SELECT * FROM user WHERE email = '$email'";
+    //echo $q;
+    //$db->query($q);
+    
+    //prepared statements
+    $q = $db->prepare("SELECT * FROM user WHERE email = ? LIMIT 1");
+    //podstaw wartości
+    $q->bind_param("s", $email);
+    //wykonaj
+    $q->execute();
+    $result = $q->get_result();
+    
+    $userRow = $result->fetch_assoc();
+    if($userRow == null) {
+        //konto nie istnieje
+        echo "Błędny login lub hasło <br>";
+    } else {
+        //konto istnieje
+        if(password_verify($password, $userRow['passwordHash'])) {
+            //hasło poprawne
+            echo "Zalogowano poprawnie <br>";
+        } else {
+            //hasło niepoprawne
+            echo "Błędny login lub hasło <br>";
+        }
+    }
+}
+
+
+
+
+?>
+<form action="index.php" method="get">
+    <label for="emailInput">Email:</label>
+    <input type="email" name="email" id="emailInput">
+    <label for="passwordInput">Hasło:</label>
+    <input type="password" name="password" id="passwordInput">
+    <input type="submit" value="Zaloguj"> 
+</form>
